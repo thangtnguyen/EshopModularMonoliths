459b0e3d0b678a79c712e94d2beedc50723b45da
Thang Nguyen <thangnguyen0722@gmail.com>
2024-10-16 13:07:03 +0700

    Section16-Develop Basket Module with VSA and CQRS

---
+++ src/Modules/Basket/Basket/Basket/Models/ShoppingCartItem.cs
@@ -0,0 +1,23 @@
+﻿namespace Basket.Basket.Models
+{
+    public class ShoppingCartItem : Entity<Guid>
+    {
+        public Guid ShoppingCartId { get; private set; } = default!;
+        public Guid ProductId { get; private set; } = default!;
+        public int Quantity { get; internal set; } = default!;
+        public string Color { get; private set; } = default!;
+
+        public decimal Price { get; private set; } = default!;
+        public string ProductName { get; private set; } = default!;
+
+        internal ShoppingCartItem(Guid shoppingCartId, Guid productId, int quantity, string color, decimal price, string productName)
+        {
+            ShoppingCartId = shoppingCartId;
+            ProductId = productId;
+            Quantity = quantity;
+            Color = color;
+            Price = price;
+            ProductName = productName;
+        }
+    }
+}
